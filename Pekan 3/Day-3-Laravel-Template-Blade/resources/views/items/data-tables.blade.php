ea169bbbd8a1ef5d5b8023adf25c57909cd3cf38
Ridwan Aji Pamungkas <droidaji@gmail.com>
2020-08-06 14:09:26 +0700

    Tugas laravel dengan template blade

--- Pekan 3/Day-3-Laravel-Template-Blade/resources/views/items/data-tables.blade.php
+++ Pekan 3/Day-3-Laravel-Template-Blade/resources/views/items/data-tables.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 @section('content')
+<!-- Card Header -->
 <div class="card">
   <div class="card-header">
     <h3 class="card-title">DataTable with default features</h3>
@@ -440,9 +441,15 @@
 </div>
 @endsection
 
-@push('script')
-<script src="../../plugins/datatables/jquery.dataTables.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+@push('style')
+<!-- DataTables -->
+<link rel="stylesheet" href="{{asset('/adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('/adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+@endpush
+
+@push('scripts')
+<script src="{{asset('/adminlte/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
 <script>
   $(function() {
     $("#example1").DataTable();
--- Pekan 3/Day-3-Laravel-Template-Blade/resources/views/layouts/master.blade.php
+++ Pekan 3/Day-3-Laravel-Template-Blade/resources/views/layouts/master.blade.php
@@ -7,11 +7,14 @@
   <title>AdminLTE 3 | Blank Page</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  @stack('style')
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('/adminlte/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{asset('/adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{asset('/adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css')}}">
   <!-- Google Font: Source Sans Pro -->
@@ -53,13 +56,14 @@
   <!-- ./wrapper -->
 
   <!-- jQuery -->
-  <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
+
+  <script src="{{ asset('/adminlte/plugins/jquery/jquery.min.js')}}"></script>
   <!-- Bootstrap 4 -->
-  <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
   <!-- AdminLTE App -->
-  <script src="{{ asset('adminlte/dist/js/adminlte.min.js')}}"></script>
+  <script src="{{ asset('/adminlte/dist/js/adminlte.min.js')}}"></script>
   <!-- AdminLTE for demo purposes -->
-  <script src="{{ asset('adminlte/dist/js/demo.js')}}"></script>
+  <script src="{{ asset('/adminlte/dist/js/demo.js')}}"></script>
 
   @stack('scripts')
 </body>
